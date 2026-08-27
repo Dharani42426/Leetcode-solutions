92e79ba6c0cdfa6f43fcc807bc58c899b7a627d7
Dharani Baliga <dharanibaliga315@gmail.com>
2026-08-27 18:53:51 +0530

    Add Easy Java Solution for 674. Longest Continuous Increasing Subsequence from LeetCode

--- LeetCode/Java/Easy/674. Longest Continuous Increasing Subsequence/Solution.java
+++ LeetCode/Java/Easy/674. Longest Continuous Increasing Subsequence/Solution.java
@@ -1,16 +1,20 @@
 class Solution {
     public int findLengthOfLCIS(int[] nums) {
-        int count = 1;
-        int max = 0;
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i]<nums[i+1]){
-                count++;
+
+        int current = 1;
+        int max = 1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] < nums[i + 1]) {
+                current++;
+            } else {
+                current = 1;
             }
-            else{
-                max = Math.max(max, count);
-                count = 0;
-            }
+
+            max = Math.max(max, current);
         }
-        return Math.max(max, count);
+
+        return max;
     }
 }
