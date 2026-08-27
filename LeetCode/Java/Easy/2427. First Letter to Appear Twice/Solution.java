2e900351b5b5003b599ca15b9fa1bd5f0d6a663c
Dharani Baliga <dharanibaliga315@gmail.com>
2026-08-27 18:54:24 +0530

    Add Easy Java Solution for 2427. First Letter to Appear Twice from LeetCode

---
+++ LeetCode/Java/Easy/2427. First Letter to Appear Twice/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            if(map.containsKey(ch)){
+                return ch;
+            }
+            map.put(ch, 1);
+        }
+        return 0;
+    }
+}
